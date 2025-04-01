aea8e52b984a90d2638198c58b6e413ed8cc715a
Jose Morales <33831975+jomoqui@users.noreply.github.com>
2025-04-01 10:49:20 +0200

    10

--- pages/4_Estilo_Cultural.py
+++ pages/4_Estilo_Cultural.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('4. Estilo Cultural')
 
 st.markdown('### Estabilidad vs. Cambio')
-estabilidad_cambio_slider_val = st.slider('', 0, 100, 50, step=25, label_visibility='collapsed', key='estabilidad_cambio_slider')
+estabilidad_cambio_slider_val = st.slider('', 0, 100, 50, step=10, label_visibility='collapsed', key='estabilidad_cambio_slider')
 st.session_state['Estabilidad vs. Cambio'] = estabilidad_cambio_slider_val
 cols = st.columns(5)
 with cols[0]:
@@ -18,7 +18,7 @@ with cols[4]:
     st.markdown(f'''<div style="padding:10px; background:#f9f9f9; border:1px solid #ddd; border-radius:10px; text-align:center;">Cultura en constante evolución, se espera agilidad y transformación continua.</div>''', unsafe_allow_html=True)
 
 st.markdown('### Independencia vs. Interdependencia')
-independencia_interdep_slider_val = st.slider('', 0, 100, 50, step=25, label_visibility='collapsed', key='independencia_interdep_slider')
+independencia_interdep_slider_val = st.slider('', 0, 100, 50, step=10, label_visibility='collapsed', key='independencia_interdep_slider')
 st.session_state['Independencia vs. Interdependencia'] = independencia_interdep_slider_val
 cols = st.columns(5)
 with cols[0]:
